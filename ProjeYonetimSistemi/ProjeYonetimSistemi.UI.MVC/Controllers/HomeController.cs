f5d6f37990a42e951039f074a59d72bcee1a3cb8
BATUHAN BARAKALI <22110002047@stu.yasar.edu.tr>
2024-06-25 18:42:20 +0300

    FrontEnd Tarafında Çalışmalar Yapıldı

--- ProjeYonetimSistemi/ProjeYonetimSistemi.UI.MVC/Controllers/HomeController.cs
+++ ProjeYonetimSistemi/ProjeYonetimSistemi.UI.MVC/Controllers/HomeController.cs
@@ -1,32 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿#region NAMESPACES
+using Microsoft.AspNetCore.Mvc;
 using ProjeYonetimSistemi.UI.MVC.Models;
 using System.Diagnostics;
+#endregion
+
 
 namespace ProjeYonetimSistemi.UI.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        #region FIELDS
         private readonly ILogger<HomeController> _logger;
-
+        #endregion
+ 
+        #region CTOR
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+        #endregion
 
+        #region ACTION RESULTS
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #endregion
+
     }
 }
---
+++ ProjeYonetimSistemi/ProjeYonetimSistemi.UI.MVC/Controllers/ProjectYonetim.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjeYonetimSistemi.UI.MVC.Controllers
+{
+    public class Project : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
