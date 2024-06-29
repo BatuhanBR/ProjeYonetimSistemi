6bfcad575ea9480ba48082a140e82efa8ba6a4dd
BATUHAN BARAKALI <22110002047@stu.yasar.edu.tr>
2024-06-29 22:30:48 +0300

    Asp.net Core Identity Altyapısı Eklendi

---
+++ ProjeYonetimSistemi/ProjeYonetimSistemi.UI.MVC/Models/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjeYonetimSistemi.UI.MVC.Models
+
+{
+    public class LoginViewModel
+    {
+        public IdentityUser Email { get; internal set; }
+        public string Password { get; internal set; }
+       
+        public bool RememberMe { get; internal set; }
+
+    }
+    
+     
+
+    
+namespace IdentityMvcApp.Models
+    {
+        public class LoginViewModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+    }
+
+}
+
+ 
--- ProjeYonetimSistemi/ProjeYonetimSistemi.UI.MVC/Program.cs
+++ ProjeYonetimSistemi/ProjeYonetimSistemi.UI.MVC/Program.cs
@@ -1,4 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProjeYonetimSistemi.UI.MVC.Context;
+using Microsoft.Extensions.DependencyInjection;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -9,7 +24,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+ 
     app.UseHsts();
 }
 
