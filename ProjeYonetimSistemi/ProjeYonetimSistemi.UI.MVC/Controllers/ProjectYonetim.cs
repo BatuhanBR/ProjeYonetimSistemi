144beb82f9e5629c0da194b070d64ca85ee2533a
BATUHAN BARAKALI <22110002047@stu.yasar.edu.tr>
2024-07-22 16:15:12 +0300

    Proje kısmının her şeyi halledildi.

--- ProjeYonetimSistemi/ProjeYonetimSistemi.UI.MVC/Controllers/ProjectYonetim.cs
+++ ProjeYonetimSistemi/ProjeYonetimSistemi.UI.MVC/Controllers/ProjectYonetim.cs
@@ -6,7 +6,6 @@ using ProjeYonetimSistemi.UI.MVC.Entity;
 
 #endregion
 
-
 namespace ProjeYonetimSistemi.UI.MVC.Controllers
 {
     public class ProjectController : Controller
@@ -23,31 +22,23 @@ namespace ProjeYonetimSistemi.UI.MVC.Controllers
         }
         #endregion
 
-
         public IActionResult Index()
         {
             var result = _context.Projects.ToList();
-
             return View(result);
         }
-        public IActionResult Detail(int id)
-        {
-            var project = _context.Projects
-                .Where(p => p.Id == id)
-                .Select(p => new
-                {
-                    p.ProjectName,
-                    p.Description
-                })
-                .FirstOrDefault();
 
+        public async Task<IActionResult> Detail(int id)
+        {
+            var project = await _context.Projects.FindAsync(id);
             if (project == null)
             {
                 return NotFound();
             }
-
             return View(project);
         }
+
+        [HttpGet]
         public async Task<IActionResult> Update(int id)
         {
             var project = await _context.Projects.FindAsync(id);
@@ -55,51 +46,61 @@ namespace ProjeYonetimSistemi.UI.MVC.Controllers
             {
                 return NotFound();
             }
-
             return View(project);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Update(int id, ProjectEntity updatedProject)
+        public async Task<IActionResult> UpdateProject(ProjectEntity updatedProject)
         {
-            if (id != updatedProject.Id)
+            if (!ModelState.IsValid)
             {
-                return BadRequest();
+                var errors = ModelState.Values.SelectMany(v => v.Errors);
+                foreach (var error in errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);  // Hataları konsola yazdırın
+                }
+                return View(updatedProject);
             }
 
-            if (ModelState.IsValid)
+            try
             {
-                try
+                var existingProject = await _context.Projects.FindAsync(updatedProject.Id);
+                if (existingProject == null)
                 {
-                    _context.Update(updatedProject);
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Index));
+                    return NotFound();
                 }
-                catch (DbUpdateConcurrencyException)
+
+                existingProject.ProjectName = updatedProject.ProjectName;
+                existingProject.TeamMembers = updatedProject.TeamMembers;
+                existingProject.Description = updatedProject.Description;
+                existingProject.CreatedDate = updatedProject.CreatedDate;
+
+                _context.Projects.Update(existingProject);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(updatedProject.Id))
                 {
-                    if (!ProjectExists(updatedProject.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
                 }
             }
-            return View(updatedProject);
         }
 
-        private bool ProjectExists(int id)
-        {
-            return _context.Projects.Any(e => e.Id == id);
-        }
+      
+
+
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var project = await _context.Projects.FindAsync(id); // Burada Projects kullanılıyor.
+            var project = await _context.Projects.FindAsync(id);
             if (project == null)
             {
                 return NotFound();
@@ -107,31 +108,31 @@ namespace ProjeYonetimSistemi.UI.MVC.Controllers
 
             _context.Projects.Remove(project);
             await _context.SaveChangesAsync();
-
             return RedirectToAction(nameof(Index));
         }
-
 
         [HttpGet]
         public IActionResult AddProject()
         {
             return View();
-
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult AddProject(ProjectEntity project)
+        public async Task<IActionResult> AddProject(ProjectEntity project)
         {
             if (ModelState.IsValid)
             {
                 _context.Projects.Add(project);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(project);
         }
 
-
+        private bool ProjectExists(int id)
+        {
+            return _context.Projects.Any(e => e.Id == id);
+        }
     }
-
 }
